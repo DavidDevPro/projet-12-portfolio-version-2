6d6bb5cd85a7a42b56e4a2d98cadf4f7b5730238
David Changea <14ir155@gmail.com>
2024-02-08 10:19:40 +0100

    start projet, routing mis en place

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import Projets from "./pages/Projets";
+import Competences from "./pages/Competences";
+import Contact from "./pages/Contact";
 
 const App = () => {
-  return <div>react-vite</div>;
+  return (
+    <div>
+      {/* particles js */}
+
+      {/* navigation */}
+
+      {/* main page content */}
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projets" element={<Projets />} />
+        <Route path="/competences" element={<Competences />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/Projets.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projets = () => {
+  return <div className="projets"></div>;
+};
+
+export default Projets;
