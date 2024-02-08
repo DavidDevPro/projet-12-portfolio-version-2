267e30dca64349b85f2fe8f85f16e612a85dbf3e
David Changea <14ir155@gmail.com>
2024-02-09 00:08:44 +0100

    section projet en cours

--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsMouse3 } from "react-icons/bs";
 import PageHead from "../components/PageHead";
+import DataProjets from "../data/projets.json";
+import filterData from "../data/filter.json";
 
 const Projets = () => {
+  const [filteredvalue, setFilteredValue] = useState(1);
+  const [hoveredValue, setHoveredValue] = useState(null);
+
+  function handleFilter(currentId) {
+    setFilteredValue(currentId);
+  }
+
+  function handleHover(index) {
+    setHoveredValue(index);
+  }
+
+  const filteredItems =
+    filteredvalue === 1
+      ? DataProjets
+      : DataProjets.filter((item) => item.id === filteredvalue);
+
   return (
-    <section id="projets" className="projetsPage">
+    <section id="projetsPage" className="projetsPage">
       <PageHead headerText="Mes projets" icon={<BsMouse3 size={45} />} />
+
+      <div className="projetsPage__content">
+        <ul className="projetsPage__content__filter">
+          {filterData.map((item) => (
+            <li
+              className={item.filterId === filteredvalue ? "active" : ""}
+              onClick={() => handleFilter(item.filterId)}
+              key={item.filterId}
+            >
+              {item.label}
+            </li>
+          ))}
+        </ul>
+        <div className="projetsPage__content__cards">
+          {filteredItems.map((item, index) => (
+            <div
+              className="projetsPage__content__cards__item"
+              key={`cardItem${item.title.trim()}`}
+              onMouseEnter={() => handleHover(index)}
+              onMouseLeave={() => handleHover(null)}
+            >
+              <div className="projetsPage__content__cards__item__img-wrapper">
+                <a>
+                  <img src={item.picture} alt={`image du ${item.title}`} />
+                </a>
+              </div>
+              <div className="overlay">
+                {index === hoveredValue && (
+                  <div>
+                    <p>{item.title}</p>
+                    <button>Visiter</button>
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
