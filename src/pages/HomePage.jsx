426e278814f32406cb5efebb578311fe12fdde9b
David Changea <14ir155@gmail.com>
2024-02-10 14:11:52 +0100

    page d'accueil version 1 terminée

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import background from "../assets/img/homeBackground.jpg";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <div className="homePage"></div>;
+  return (
+    <div className="homePage">
+      <img src={background} alt="image de la page d'accueil" />
+      <div className="homePage__container">
+        <div className="homePage__container__text">
+          <h2>Bonjour,</h2>
+          <div className="homePage__container__text__name">
+            <p>Je suis </p>
+            <span>David</span>
+          </div>
+          <p>Développeur Front-End</p>
+          <Link to="/contact">
+            <button>Contactez-moi</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
